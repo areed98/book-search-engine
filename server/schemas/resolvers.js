fbe1b3c14faaa3470cf61be3ef196418d058d1d7
areed98 <austinjreed89@gmail.com>
2022-12-29 15:24:32 -0500

    finished schemas, edited server.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,79 @@
+//Imports
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const { sign } = require('jsonwebtoken');
+
+//Resolvers
+const resolvers = {
+    //Query the resolvers
+    Query: {
+        me: async (parent, args, context) => {
+        if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            .populate('savedBooks')
+            return userData;
+            }
+            //Auth error
+            throw new AuthenticationError('You are not logged in');
+        }
+    },
+    //Mutations
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            //Login w/ arguments
+            const user = await User.findOne({ email });
+            //If no user, auth error
+            if (!user) {
+                throw new AuthenticationError('Wrong username!');
+            }
+            //Password auth
+            const correctPw = await user.isCorrectPassword(password);
+            //If incorrect, auth error
+            if (!correctPw) {
+                throw new AuthenticationError('Wrong password!');
+            }
+            //Token
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (partent, args) => {
+            //user creation
+            const user = await User.create(args);
+            //user token
+            const token = signToken(user);
+            //return
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            //save book if user is logged in
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate (
+                    { _id: context.user._id },
+                    { $addToSet: {savedBooks: args} },
+                    { new: true, runValidators: true }
+                );
+                return updatedUser;
+            }
+            //login error
+            throw new AuthenticationError('You must be logged in!');
+        },
+        deleteBook: async (parent, { bookId }, context) => {
+            //delete book if user is logged in
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate (
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            //login error
+            throw new AuthenticationError('You must be logged in!');
+        }
+    }
+};
+
+//export resolver
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 // Apollo server setup
 const { ApolloServer } = require('apollo-server-express');
@@ -25,7 +25,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/*', (req, res) => {
+  res.sendFile(path,join(__dirname, '../client/build/index.html'))
+})
 
 //Apollo server function
 const startApolloServer = async () => {
